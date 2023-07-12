5b5f4cfb411101155b150d29c561244e25a4abb1
Bradley <Bradleywinson1996@gmail.com>
2023-07-12 22:29:11 +0100

    Add project files.

---
+++ WeatherApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherData data = new WeatherData();
+            Console.WriteLine("Enter a Country: ");
+            string cityName = Console.ReadLine();
+            Console.WriteLine("City Name Entered: " + cityName);
+            data.GetWeatherData(cityName);
+
+
+        }
+    }
+}
---
+++ WeatherApplication/WeatherData.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApplication
+{
+    internal class WeatherData
+    {
+        public string GetWeatherData(string cityName) 
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            IWebElement ele2 = driver.FindElement(By.Id("W0wltc"));
+            ele2.Click();
+            IWebElement ele = driver.FindElement(By.Name("q"));
+            ele.SendKeys(cityName + " Weather");
+            ele.Submit();
+            IWebElement Temp = driver.FindElement(By.Id("wob_tm"));
+            IWebElement TimenDate = driver.FindElement(By.Id("wob_dts"));
+            IWebElement WeatherType = driver.FindElement(By.Id("wob_dc"));
+            string result = cityName + " is " + Temp.Text + " Degrees " + TimenDate.Text + " " + WeatherType.Text;
+            driver.Close();
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
